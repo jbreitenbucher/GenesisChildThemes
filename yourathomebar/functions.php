5ee934486716f668982d83af5c3f48a40e5b4178
jbreitenbucher <jon@breitenbucher.net>
2011-05-10 02:01:52 -0400

    Added code to display featured images to the posts and pages screens. Also added code to remove the right header widget area.

--- yourathomebar/functions.php
+++ yourathomebar/functions.php
@@ -5,9 +5,6 @@ require_once(TEMPLATEPATH.'/lib/init.php');
 /** Remove the right header widget area **/
 remove_action ('genesis_do_header','header_right');
 
-/** Remove the navigation area **/
-// remove_action('genesis_after_header', 'genesis_do_nav');
-
 /**  Remove footer **/
 remove_action('genesis_footer', 'genesis_do_footer'); 
 remove_action('genesis_footer', 'genesis_footer_markup_open', 5); 
@@ -23,10 +20,10 @@ if ( function_exists( 'add_theme_support' ) ) {
 	set_post_thumbnail_size( 290, 179, true );
 }
 
-// Add new image sizes
+/** Add new image sizes for the Dynamic Content Gallery **/
 add_image_size('Dynamic Content Gallery', 610, 225, TRUE);
 
-// Force layout on homepage
+/** Force layout on homepage **/
 add_filter('genesis_pre_get_option_site_layout', 'yourathomebar_home_layout');
 function yourathomebar_home_layout($opt) {
 	if ( is_home() )
@@ -34,7 +31,7 @@ function yourathomebar_home_layout($opt) {
 	return $opt;
 }
 
-// Replaces homepage sidebar with Sidebar Home in widget area
+/** Replaces homepage sidebar with Sidebar Home in widget area **/
 add_action('genesis_after_content', 'get_custom_sidebar', 5);
 function get_custom_sidebar() {
     if( is_home() ) {
@@ -43,7 +40,7 @@ function get_custom_sidebar() {
 	}
 }
 
-// Register widget areas
+/** Register widget areas **/
 genesis_register_sidebar(array(
 	'name'=>'Sidebar Home',
 	'id' => 'sidebar-home',
@@ -69,103 +66,19 @@ genesis_register_sidebar(array(
 	'before_title'=>'<h4 class="widgettitle">','after_title'=>'</h4>'
 ));
 
-/** Create a Drink Receipes Post Type 
-add_action( 'init', 'drink_recipes_register' );
-function drink_recipes_register() {
-	
-	$labels = array(
-			'name' => _x('Drink Recipes', 'post type general name'),
-			'singular_name' => _x('Drink Recipe', 'post type singular name'),
-			'add_new' => _x('Add New', 'portfolio item'),
-			'add_new_item' => __('Add New Recipe'),
-			'edit_item' => __('Edit Recipe'),
-			'new_item' => __('New Recipe'),
-			'view_item' => __('View Recipe'),
-			'search_items' => __('Search Recipes'),
-			'not_found' =>  __('Nothing found'),
-			'not_found_in_trash' => __('Nothing found in Trash'),
-			'parent_item_colon' => ''
-		);
-		
-	$args = array(
-			'labels' => $labels,
-			'public' => true,
-			'publicly_queryable' => true,
-			'show_ui' => true,
-			'query_var' => true,
-			'rewrite' => true,
-			'capability_type' => 'post',
-			'hierarchical' => false,
-			'has_archive' => 'drink-recipes',
-			'menu_position' => 5,
-			'supports' => array('title','editor','thumbnail'),
-			'taxonomies' => array( 'category')
-		  );
-		
-	register_post_type( 'drinkrecipes', $args);
-} **/
+function remove_some_sidebars(){
+	// Unregsiter some of the Genesis sidebars
+	unregister_sidebar( 'header_right' );
+}
+add_action( 'widgets_init', 'remove_some_sidebars', 11 );
 
+/** Create a custom taxonomy for drink ingredients **/
 register_taxonomy("Ingredients", array("post"), array("hierarchical" => false, "label" => "Ingredients", "singular_label" => "Ingredient", "rewrite" => true));
 
 /** Add support for asides to use for drinking facts **/
 add_theme_support( 'post-formats', array( 'aside','video' ) );
 
-/** Create a Drinking Facts Post Type 
-add_action( 'init', 'drinking_facts_register' );
-function drinking_facts_register() {
-	
-	$labels = array(
-			'name' => _x('Drinking Facts', 'post type general name'),
-			'singular_name' => _x('Drinking Fact', 'post type singular name'),
-			'add_new' => _x('Add New', 'portfolio item'),
-			'add_new_item' => __('Add New Drinking Fact'),
-			'edit_item' => __('Edit Drinking Fact'),
-			'new_item' => __('New Drinking Fact'),
-			'view_item' => __('View Drinking Facts'),
-			'search_items' => __('Search Drinking Facts'),
-			'not_found' =>  __('Nothing found'),
-			'not_found_in_trash' => __('Nothing found in Trash'),
-			'parent_item_colon' => ''
-		);
-		
-	$args = array(
-			'labels' => $labels,
-			'public' => true,
-			'publicly_queryable' => true,
-			'show_ui' => true,
-			'query_var' => true,
-			'rewrite' => true,
-			'capability_type' => 'post',
-			'hierarchical' => false,
-			'has_archive' => 'drinking-facts',
-			'menu_position' => 6,
-			'supports' => array('title','editor')
-		  );
-		
-	register_post_type( 'drinkingfacts', $args);
-}
-
-add_post_type_support( 'drinkingfacts', 'post-formats', array( 'aside' ) );
-
-add_action( 'admin_head', 'cpt_icons' );
-function cpt_icons() {
-    ?>
-    <style type="text/css" media="screen">
-        #menu-posts-drinkrecipes .wp-menu-image {
-            background: url(<?php bloginfo('stylesheet_directory') ?>/images/address-book-open.png) no-repeat 6px -17px !important;
-        }
-	#menu-posts-drinkrecipes:hover .wp-menu-image, #menu-posts-drinkrecipes.wp-has-current-submenu .wp-menu-image {
-            background-position:6px 7px!important;
-        }
-				#menu-posts-drinkingfacts .wp-menu-image {
-            background: url(<?php bloginfo('stylesheet_directory') ?>/images/glass.png) no-repeat 6px -17px !important;
-        }
-	#menu-posts-drinkingfacts:hover .wp-menu-image, #menu-posts-drinkingfacts.wp-has-current-submenu .wp-menu-image {
-            background-position:6px 7px!important;
-        }
-    </style>
-<?php } **/
-
+/** Create a custom widget for displaying a tag cloud of Ingredients **/
 add_action("widgets_init", array('Widget_Custom_tax_tag_cloud', 'register'));
 class Widget_Custom_tax_tag_cloud {
     function control(){
@@ -182,4 +95,42 @@ class Widget_Custom_tax_tag_cloud {
         register_sidebar_widget('Drinks by Ingredient', array('Widget_Custom_tax_tag_cloud', 'widget', 'before_widget' => '<li id="%1$s" class="widget %2$s">', 'after_widget'  => '</li>', 'before_title'  => '<h4 class="widgettitle">', 'after_title'   => '</h4>'));
         register_widget_control('Drinks by Ingredient', array('Widget_Custom_tax_tag_cloud', 'control'));
     }
+}
+
+/** Add Thumbnails in Manage Posts/Pages List **/
+if ( !function_exists('AddThumbColumn') && function_exists('add_theme_support') ) {
+    // for post and page
+    add_theme_support('post-thumbnails', array( 'post', 'page' ) );
+    function AddThumbColumn($cols) {
+        $cols['thumbnail'] = __('Thumbnail');
+        return $cols;
+    }
+    function AddThumbValue($column_name, $post_id) {
+            $width = (int) 60;
+            $height = (int) 60;
+            if ( 'thumbnail' == $column_name ) {
+                // thumbnail of WP 2.9
+                $thumbnail_id = get_post_meta( $post_id, '_thumbnail_id', true );
+                // image from gallery
+                $attachments = get_children( array('post_parent' => $post_id, 'post_type' => 'attachment', 'post_mime_type' => 'image') );
+                if ($thumbnail_id)
+                    $thumb = wp_get_attachment_image( $thumbnail_id, array($width, $height), true );
+                elseif ($attachments) {
+                    foreach ( $attachments as $attachment_id => $attachment ) {
+                        $thumb = wp_get_attachment_image( $attachment_id, array($width, $height), true );
+                    }
+                }
+                    if ( isset($thumb) && $thumb ) {
+                        echo $thumb;
+                    } else {
+                        echo __('None');
+                    }
+            }
+    }
+    // for posts
+    add_filter( 'manage_posts_columns', 'AddThumbColumn' );
+    add_action( 'manage_posts_custom_column', 'AddThumbValue', 10, 2 );
+    // for pages
+    add_filter( 'manage_pages_columns', 'AddThumbColumn' );
+    add_action( 'manage_pages_custom_column', 'AddThumbValue', 10, 2 );
 }
