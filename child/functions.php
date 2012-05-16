05b9c84cc274c9abdd0368787b9ba493ee80550d
jbreitenbucher <jon@breitenbucher.net>
2012-05-16 11:14:47 -0400

    Update Genesis Sample Child Theme

--- child/functions.php
+++ child/functions.php
@@ -1,8 +1,22 @@
 <?php
-/** Start the engine **/
-require_once(TEMPLATEPATH.'/lib/init.php');
+/** Start the engine */
+require_once( get_template_directory() . '/lib/init.php' );
 
-/** Add support for custom background **/
-if (function_exists('add_custom_background')) {
-	add_custom_background();
+/** Child theme (do not remove) */
+define( 'CHILD_THEME_NAME', 'Sample Child Theme' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/themes/genesis' );
+
+/** Add Viewport meta tag for mobile browsers */
+add_action( 'genesis_meta', 'add_viewport_meta_tag' );
+function add_viewport_meta_tag() {
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
 }
+
+/** Add support for custom background */
+add_custom_background();
+
+/** Add support for custom header */
+add_theme_support( 'genesis-custom-header', array( 'width' => 960, 'height' => 100 ) );
+
+/** Add support for 3-column footer widgets */
+add_theme_support( 'genesis-footer-widgets', 3 );
