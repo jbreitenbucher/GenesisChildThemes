d8267788c62f9cd8ff9d40e5184e9ede81c2b2fd
jbreitenbucher <jon@breitenbucher.net>
2012-12-20 19:25:49 -0500

    Updated homepage to use Genesis 1.8 features

--- landscape-2/home.php
+++ landscape-2/home.php
@@ -1,10 +1,48 @@
-<?php get_header(); ?>
-<?php genesis_home(); ?>
+<?php
+/**
+ * Home - Used for the homepage
+ *
+ * @package     landscape
+ * @author      Jon Breitenbucher <jon@breitenbucher.net>
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ *
+ */
 
-<div id="homepage">
-	<?php if( function_exists('wp_cycle') ) : ?>
-		<?php wp_cycle(); ?>
-	<?php endif; ?>
-</div>
+/**
+ * Add widget support for homepage. If no widgets active, display the default loop.
+ *
+ */
 
-<?php get_footer(); ?>
+add_action( 'genesis_meta', 'landscape_home_genesis_meta' );
+function landscape_home_genesis_meta() {
+
+    // If any homepage widget area contains widgets use a custom loop
+    //if ( is_active_sidebar( 'featured' ) ) {
+        
+        // Customize the loop
+        remove_action( 'genesis_loop', 'genesis_do_loop' );
+        add_action( 'genesis_loop', 'landscape_home_loop_helper' );
+        add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
+    //}
+}
+
+/**
+ * Customize Post Content
+ *
+ * @author Jon Breitenbucher
+ */
+
+function landscape_home_loop_helper() {
+	echo '<div id="homepage">';
+		if( function_exists('wp_cycle') ) {
+			wp_cycle();
+		} else {
+			if ( is_active_sidebar( 'homepage-image' ) ) {
+				dynamic_sidebar( 'homepage-image' );
+			}
+		}
+	echo '</div>';
+}
+
+genesis();
