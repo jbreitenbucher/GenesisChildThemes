72b1513806a3d7b2511495a318e93fbc013398a3
jbreitenbucher <jon@breitenbucher.net>
2013-02-07 09:21:38 -0500

    updating wooster-sga to remove deprecated function call add_custom_background()

--- wooster-sga/functions.php
+++ wooster-sga/functions.php
@@ -3,6 +3,4 @@
 require_once(TEMPLATEPATH.'/lib/init.php');
 
 /** Add support for custom background **/
-if (function_exists('add_custom_background')) {
-	add_custom_background();
-}
+add_theme_support( 'custom-background' );
