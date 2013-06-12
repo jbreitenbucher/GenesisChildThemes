e61ccd455debe8068876d7eed9a16da2602fdbfb
Jon Breitenbucher <jon@breitenbucher.net>
2013-06-12 12:48:17 -0400

    Add widget area for slider, social sharing, and add custom header

--- ginni/functions.php
+++ ginni/functions.php
@@ -3,8 +3,8 @@
 require_once( get_template_directory() . '/lib/init.php' );
 
 // Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
-define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_NAME', 'Ginni Child Theme' );
+define( 'CHILD_THEME_URL', 'http://thepedestalgroup.com' );
 
 // Add Viewport meta tag for mobile browsers
 add_action( 'genesis_meta', 'sample_viewport_meta_tag' );
@@ -15,178 +15,128 @@ function sample_viewport_meta_tag() {
 // Add support for custom background
 add_theme_support( 'custom-background' );
 
-// Add support for custom header
-add_theme_support( 'genesis-custom-header', array(
-	'width' => 1152,
-	'height' => 120
-) );
+// Customize the Genesis Custom Header to use the Featured Image
+set_post_thumbnail_size( 1152, 120, true ); /*Set the size of thumbnails to that of our header */
+function jb_custom_header_style() {
+	global $post;
+	if ( is_singular() &&
+			has_post_thumbnail( $post->ID ) &&
+			( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'interior-header' ) ) ):
+			echo '<div id="featured-image"><img src="' . $image[0] . '" /></div>';
+	else :
+			return;
+	endif;
+}
 
-// Add support for 3-column footer widgets
+// Add support for custom header
+add_theme_support( 'genesis-custom-header', array( 'width' => 1152, 'height' => 120 ) );
+
+add_action( 'genesis_after_header', 'jb_custom_header_style', 12 );
+
+// Custom image sizes
+add_image_size( 'slider', 1110, 300, TRUE );
+add_image_size( 'home-posts', 350, 262, TRUE );
+add_image_size( 'interior-header', 1152, 120, TRUE );
+
+// Add Featured Image to Posts
+add_action( 'genesis_after_post_content', 'child_do_single_post_image' );
+function child_do_single_post_image() {
+
+    if( is_home() )
+        genesis_image( array( 'size' => 'home-posts', 'attr' => array( 'class' => 'aligncenter' ) ) );
+}
+
+// Remove Custom Menu support
+remove_theme_support ( 'genesis-menus' );
+// Default Menus: registers menus
+add_theme_support ( 'genesis-menus' , array ( 'primary' => 'Primary Navigation Menu' , 'secondary' => 'Secondary Navigation Menu' ,'tertiary' => 'Tertiary Navigation Menu' ) );
+
+// Move secondary nav menu */
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_before_header', 'genesis_do_subnav', 12 );
+
+// Add new navbar
+add_action('genesis_before_header', 'tertiary', 11);
+function tertiary() {
+require( get_stylesheet_directory() .'/tertiary.php');
+}
+
+// Add support for 5-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 5 );
 
+// Remove the post info function
+remove_action( 'genesis_before_post_content', 'genesis_post_info' );
+
+// Remove the post meta function
+remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
+
+// Remove the pagination function
+remove_action( 'genesis_entry_content', 'genesis_do_post_content_nav' );
+
 /**
- * Grid Loop Pagination
- * Returns false if not grid loop.
- * Returns an array describing pagination if is grid loop
+ * Add widget support for homepage. If no widgets active, display the default loop.
  *
- * @author Bill Erickson
- * @link http://www.billerickson.net/a-better-and-easier-grid-loop/
- *
- * @param object $query
- * @return bool is grid loop (true) or not (false)
  */
-function be_grid_loop_pagination( $query = false ) {
- 
-	// If no query is specified, grab the main query
-	global $wp_query;
-	if( !isset( $query ) || empty( $query ) || !is_object( $query ) )
-		$query = $wp_query;
-		
-	// Sections of site that should use grid loop	
-	if( ! ( $query->is_home() || $query->is_archive() ) )
-		return false;
-		
-	// Specify pagination
-	return array(
-		'features_on_front' => 5,
-		'teasers_on_front' => 6,
-		'features_inside' => 0,
-		'teasers_inside' => 12,
-	);
-}
- 
-/**
- * Grid Loop Query Arguments
- *
- * @author Bill Erickson
- * @link http://www.billerickson.net/a-better-and-easier-grid-loop/
- *
- * @param object $query
- * @return null
- */
-function be_grid_loop_query_args( $query ) {
-	$grid_args = be_grid_loop_pagination( $query );
-	if( $query->is_main_query() && !is_admin() && $grid_args ) {
- 
-		// First Page
-		$page = $query->query_vars['paged'];
-		if( ! $page ) {
-			$query->set( 'posts_per_page', ( $grid_args['features_on_front'] + $grid_args['teasers_on_front'] ) );
-			
-		// Other Pages
-		} else {
-			$query->set( 'posts_per_page', ( $grid_args['features_inside'] + $grid_args['teasers_inside'] ) );
-			$query->set( 'offset', ( $grid_args['features_on_front'] + $grid_args['teasers_on_front'] ) + ( $grid_args['features_inside'] + $grid_args['teasers_inside'] ) * ( $page - 2 ) );
-			// Offset is posts on first page + posts on internal pages * ( current page - 2 )
-		}
- 
+function jb_home_genesis_meta() {
+
+	if ( is_active_sidebar( 'slider' ) ) {
+	
+		add_action( 'genesis_after_header', 'jb_home_slider' );
+
 	}
 }
-add_action( 'pre_get_posts', 'be_grid_loop_query_args' );
- 
+
 /**
- * Grid Loop Post Classes
+ * Display widget content for homepage sections.
  *
- * @author Bill Erickson
- * @link http://www.billerickson.net/a-better-and-easier-grid-loop/
- *
- * @param array $classes
- * @return array $classes
  */
-function be_grid_loop_post_classes( $classes ) {
-	global $wp_query;
-	$grid_args = be_grid_loop_pagination();
-	if( ! $grid_args )
-		return $classes;
+function jb_home_slider() {
+
+		if ( is_active_sidebar( 'slider' ) ) {
 		
-	// First Page Classes
-	if( ! $wp_query->query_vars['paged'] ) {
-	
-		// Features
-		if( $wp_query->current_post < $grid_args['features_on_front'] ) {
-			$classes[] = 'feature';
+		echo '<div class="slider">';
 		
-		// Teasers
-		} else {
-			$classes[] = 'one-third';
-			if( 0 == ( $wp_query->current_post - $grid_args['features_on_front'] ) || 0 == ( $wp_query->current_post - $grid_args['features_on_front'] ) % 3 )
-				$classes[] = 'first';
+				echo '<div class="wrap">';
+				dynamic_sidebar( 'slider' );
+				echo '</div><!-- end .wrap -->';
+				
+		echo '</div><!-- end .slider -->';
+		
 		}
+
+}
+
+add_action( 'genesis_before_footer', 'jb_social', 8 );
+/**
+ * Display widget content for social widget area.
+ *
+ */
+function jb_social() {
+
+		if ( is_active_sidebar( 'social' ) ) {
 		
-	// Inner Pages
-	} else {
- 
-		// Features
-		if( $wp_query->current_post < $grid_args['features_inside'] ) {
-			$classes[] = 'feature';
+		echo '<div class="social">';
 		
-		// Teasers
-		} else {
-			$classes[] = 'one-third';
-			if( 0 == ( $wp_query->current_post - $grid_args['features_inside'] ) || 0 == ( $wp_query->current_post - $grid_args['features_inside'] ) % 3 )
-				$classes[] = 'first';
+				echo '<div class="wrap">';
+				dynamic_sidebar( 'social' );
+				echo '</div><!-- end .wrap -->';
+				
+		echo '</div><!-- end .social -->';
+		
 		}
-	
-	}
-	
-	return $classes;
+
 }
-add_filter( 'post_class', 'be_grid_loop_post_classes' );
- 
-/**
- * Grid Image Sizes 
- *
- */
-function be_grid_image_sizes() {
-	add_image_size( 'be_grid', 175, 120, true );
-	add_image_size( 'be_feature', 570, 333, true );
-}
-add_action( 'genesis_setup', 'be_grid_image_sizes', 20 );
- 
-/**
- * Grid Loop Featured Image
- *
- * @param string image size
- * @return string
- */
-function be_grid_loop_image( $image_size ) {
-	global $wp_query;
-	$grid_args = be_grid_loop_pagination();
-	if( ! $grid_args )
-		return $image_size;
-		
-	// Feature
-	if( ( ! $wp_query->query_vars['paged'] && $wp_query->current_post < $grid_args['features_on_front'] ) || ( $wp_query->query_vars['paged'] && $wp_query->current_post < $grid_args['features_inside'] ) )
-		$image_size = 'be_feature';
-		
-	if( ( ! $wp_query->query_vars['paged'] && $wp_query->current_post > ( $grid_args['features_on_front'] - 1 ) ) || ( $wp_query->query_vars['paged'] && $wp_query->current_post > ( $grid_args['features_inside'] - 1 ) ) )
-		$image_size = 'be_grid';
-		
-	return $image_size;
-}
-add_filter( 'genesis_pre_get_option_image_size', 'be_grid_loop_image' );
- 
-/**
- * Fix Posts Nav
- *
- * The posts navigation uses the current posts-per-page to 
- * calculate how many pages there are. If your homepage
- * displays a different number than inner pages, there
- * will be more pages listed on the homepage. This fixes it.
- *
- */
-function be_fix_posts_nav() {
-	
-	if( get_query_var( 'paged' ) )
-		return;
-		
-	global $wp_query;
-	$grid_args = be_grid_loop_pagination();
-	if( ! $grid_args )
-		return;
- 
-	$max = ceil ( ( $wp_query->found_posts - $grid_args['features_on_front'] - $grid_args['teasers_on_front'] ) / ( $grid_args['features_inside'] + $grid_args['teasers_inside'] ) ) + 1;
-	$wp_query->max_num_pages = $max;
-	
-}
-add_filter( 'genesis_after_endwhile', 'be_fix_posts_nav', 5 );
+
+/** Register Homepage Widget area */
+genesis_register_sidebar(array(
+	'name'=>'Slider',
+	'id' => 'slider',
+	'description' => 'This is the widget area on the homepage for a slider'
+));
+
+/** Register Social Widget area */
+genesis_register_sidebar(array(
+	'name'=>'Social',
+	'id' => 'social',
+	'description' => 'This is the widget area for social media'
+));
